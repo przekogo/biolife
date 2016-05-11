ac4b6304e45d946c222206e9ca4f32ed0628ea84
Przemek Rogos <przeroog@gmail.com>
2016-05-11 22:42:50 +0200

    contact front

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   post "news/render_news", to: "news#render_news"
 
   get 'about', to: 'about#index'
+  get 'offer', to: 'offer#index'
+  get 'pellet', to: 'pellet#index'
+  get 'contact', to: 'contact#index'
 end
