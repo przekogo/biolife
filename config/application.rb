93ec80ba62fca4aa64877ccb16cc98f41f4dca1b
Przemek Rogos <przeroog@gmail.com>
2016-05-12 20:57:07 +0200

    gmail configuration

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,16 @@ module Biolife
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.serve_static_files = true
-  end
+    config.action_mailer.perform_deliveries = true
+
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+        address:              'smtp.gmail.com',
+        port:                 587,
+        domain:               'gmail.com',
+        user_name:            ENV['GMAIL_ACCOUNT'],
+        password:             ENV['GMAIL_PASSWORD'],
+        authentication:       'login',
+        enable_starttls_auto: true  }
+    end
 end
