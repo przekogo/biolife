b1611606d5d0dafa7ce990397b72eb08633de79e
Przemek Rogos <przeroog@gmail.com>
2016-05-16 23:00:45 +0200

    dropdown visual update

--- app/assets/javascripts/menu.js
+++ app/assets/javascripts/menu.js
@@ -2,7 +2,7 @@ $(document).on('click', '.nav a', function(){
   $(this).blur();
 }).on('click', '#offer a', function(){
   if ($('.offers').hasClass('slidup')) {
-    $('.offers').css({'top' : '2px', 'opacity' : '1', 'z-index' : '2'});  
+    $('.offers').css({'top' : '0px', 'opacity' : '1', 'z-index' : '2'});  
   }
   else {
     $('.offers').css({'top' : '-150px', 'opacity' : '0', 'z-index' : '-1'});
