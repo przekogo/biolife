60a4ed57ff551b37e5a46e67948a6b2bfcd9fd7d
Przemek Rogos <przeroog@gmail.com>
2016-05-25 16:41:24 +0200

    furnaces views

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -2,7 +2,12 @@ class OffersController < ApplicationController
 
   def pellet ; end
 
-  def furnaces ; end
+  def furnaces
+    @items = []
+    [['Piece', 'furnaces/about'], ['KDC EKO', 'furnaces/eko'], ['KDC EKO DUO', 'furnaces/eko_duo'], ['KDC BIO PELL', 'furnaces/bio_pell']].each do |i|
+      @items << {label: i[0], file: i[1]}
+    end
+  end
 
   def modernization ; end
 
